38c6b61c9f3900da72c493db2c95b8dc211417aa
mofareed-97 <m.fareeed1997@gmail.com>
2023-06-11 15:24:41 +0300

    about section hidde on mobile

--- src/components/Profile/About.tsx
+++ src/components/Profile/About.tsx
@@ -5,7 +5,7 @@ import { UserType } from "~/pages/[userId]";
 
 function About({ profile }: { profile: UserType }) {
   return (
-    <div className="card flex flex-col  justify-center gap-6 p-10 pl-4 pr-20 ">
+    <div className="card hidden flex-col justify-center  gap-6 p-10 pl-4 pr-20 md:flex ">
       <div className="flex items-center gap-2">
         <Mail className="h-4 w-4" />
         <span className="text-xs">{profile?.email}</span>
